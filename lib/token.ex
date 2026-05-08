db7ef1d0854b5a00c74d4446ce686ab4b4253edc
pfeyz <paul@pfeyz.com>
2026-05-07 23:47:51 -0400

    make Token a private module inside Tok

--- lib/token.ex
+++ lib/token.ex
@@ -1,54 +1,49 @@
-defmodule Token do
-  @moduledoc """
+defmodule Tok do
+    @moduledoc """
 
-  A Token type is a struct that contains :val and :char keys.
+    A Token type is a struct that contains :val and :char keys.
 
-  A module that implements the Token behaviour must implement an `init` function
-  that accepts a string and returns the data structure the token is meant to
-  represent. This will be stored as :value in the struct.
+    A module that implements the Token behaviour must implement an `init` function
+    that accepts a string and returns the data structure the token is meant to
+    represent. This will be stored as :value in the struct.
 
-  :char is used to store an integer index of where the token appeared in
-  the input string to allow for informative error messages.
+    :char is used to store an integer index of where the token appeared in
+    the input string to allow for informative error messages.
 
-    iex> Tokenizer.tokenize!("1 -2 0.334 -99.1 () ( ) + / name _var ok123")
-    [
-    %Tok.Num{val: 1, char: 0},
-    %Tok.Num{val: -2, char: 2},
-    %Tok.Num{val: 0.334, char: 5},
-    %Tok.Num{val: -99.1, char: 11},
-    %Tok.Seq{val: :start, char: 17},
-    %Tok.Seq{val: :end, char: 18},
-    %Tok.Seq{val: :start, char: 20},
-    %Tok.Seq{val: :end, char: 22},
-    %Tok.Symbol{val: :+, char: 24},
-    %Tok.Symbol{val: :/, char: 26},
-    %Tok.Symbol{val: :name, char: 28},
-    %Tok.Symbol{val: :_var, char: 33},
-    %Tok.Symbol{val: :ok123, char: 38}
-    ]
+        iex> Tokenizer.tokenize!("1 -2 0.334 -99.1 () ( ) + / name _var ok123")
+        [
+        %Tok.Num{val: 1, char: 0},
+        %Tok.Num{val: -2, char: 2},
+        %Tok.Num{val: 0.334, char: 5},
+        %Tok.Num{val: -99.1, char: 11},
+        %Tok.Seq{val: :start, char: 17},
+        %Tok.Seq{val: :end, char: 18},
+        %Tok.Seq{val: :start, char: 20},
+        %Tok.Seq{val: :end, char: 22},
+        %Tok.Symbol{val: :+, char: 24},
+        %Tok.Symbol{val: :/, char: 26},
+        %Tok.Symbol{val: :name, char: 28},
+        %Tok.Symbol{val: :_var, char: 33},
+        %Tok.Symbol{val: :ok123, char: 38}
+        ]
 
-  """
+    """
 
-  @callback init(value :: String.t()) :: term
-  defmacro __using__(__ops) do
-    quote do
-      defstruct [:val, :char]
-      @behaviour Token
-      def from_string(value) do
-        %__MODULE__{val: init(value)}
+    # just "use"ed by individual Tok modules
+    defmodule Token do
+      @callback init(value :: String.t()) :: term
+      defmacro __using__(__ops) do
+        quote do
+          defstruct [:val, :char]
+          @behaviour Token
+          def from_string(value) do
+            %__MODULE__{val: init(value)}
+          end
+        end
       end
     end
-  end
-end
 
-# the tokens used in the codebase
-defmodule Tok do
-  @type t :: Op | Space | Symbol | Seq | Str | Num
-  defmodule Op do
-    @moduledoc "Operators that invoke functions, like *"
-    use Token
-    def init(value), do: String.to_atom(value)
-  end
+  @type t :: Space | Symbol | Seq | Str | Num
 
   defmodule Space do
     @moduledoc "Whitespace tokens"
